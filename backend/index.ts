ea8a5e47e490e6cb50ad8f465fc17d2b414b23ec
Justin <jayscro287@gmail.com>
2025-05-22 15:36:45 -0400

    add exec for console.exe command, add --watch to run command

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,22 @@
 const { createServer } = require('node:http');
+const { exec } = require('node:child_process');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const solverPath = process.env.SOLVER_PATH_EXE;
+console.log(solverPath);
+
+exec(`${solverPath} -i resources/text/commandline_sample_input.txt `, (error: string, stdout: string, stderr: string) => {
+    // once the command has completed, the callback function is called
+    if (error) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", error)
+        return
+    }
+    // log the output received from the command
+    console.log(stdout)
+});
 
 const server = createServer((req: any, res: any) => {
   res.statusCode = 200;
